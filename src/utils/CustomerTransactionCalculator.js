f4ba68033e328de7b79a6dc42471f15004832688
JGeorge53 <Joju.George53@T-Mobile.com>
2024-07-13 20:54:54 +0530

    style change

--- src/utils/CustomerTransactionCalculator.js
+++ src/utils/CustomerTransactionCalculator.js
@@ -3,7 +3,6 @@ import {CustomerRewardCalculator} from "./CustomerRewardCalculator";
 // Calculates the Reward points and total purchase amount of the customer transaction dataset
 export const  CustomerTransactionCalculator = (transactionDataSet)=> {
 
-
     // Returns an Object of computed customer transaction dataset
     const transactionData = transactionDataSet && transactionDataSet.reduce((res,transactionData) => {
         const {customerId, customerName, transactionId, transactionDate, amount } = transactionData;
@@ -12,13 +11,11 @@ export const  CustomerTransactionCalculator = (transactionDataSet)=> {
         // Calculate the Reward points based on the amount of purchase
         const rewardPoints = CustomerRewardCalculator(amount);
 
-
         if(!res[customerId])
             res[customerId] = {customerName:'', monthlyTransaction:{}, totalRewards:0, totalPurchase:0}
 
         if(!res[customerId].monthlyTransaction[month])
             res[customerId].monthlyTransaction[month] = {monthlyAmount:0, monthlyReward:0}
-
 
             res[customerId].monthlyTransaction[month].monthlyAmount += amount;
             res[customerId].monthlyTransaction[month].monthlyReward += rewardPoints;
